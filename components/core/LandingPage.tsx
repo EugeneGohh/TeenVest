839792ddcbb7ce5dbef527d5c00992f541adfcaa
Eugene <gzkgt11@gmail.com>
2021-07-19 17:03:50 +0800

    make a template of navbar

---
+++ components/core/LandingPage.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import NavBar from "./NavBar";
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import '@fontsource/roboto/400.css';
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#5D3AB7",
+      contrastText: "#FFFFFF",
+    },
+    text: {
+      primary: "#FFFFFF",
+      secondary: "#FFFFFF",
+    },
+  },
+});
+
+function LandingPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <NavBar />
+    </ThemeProvider>
+  );
+}
+
+export default LandingPage;
