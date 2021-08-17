c8d80276854bf1060121a7ab4e124978ca9962b7
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2021-08-17 14:17:11 +0800

    Make (#10)
    * bump materialui
    
    * fix error
    
    * remove spacing
    
    * make category
    
    * update on rowspacing

--- components/core/LandingPage.tsx
+++ components/core/LandingPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
 import NavBar from "./NavBar";
 import Header from "./Header";
+import Category from "./Category";
 import "@fontsource/roboto/400.css";
 
 const theme = createTheme({
@@ -73,6 +74,7 @@ function LandingPage() {
       <CssBaseline />
       <NavBar />
       <Header />
+      <Category />
     </ThemeProvider>
   );
 }
