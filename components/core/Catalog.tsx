f4d94a1b34ce2baee1f8cef04eb4822c7592e744
Eugene <gzkgt11@gmail.com>
2021-08-29 23:31:30 +0800

    add animation & animation libraries

--- components/core/Catalog.tsx
+++ components/core/Catalog.tsx
@@ -8,6 +8,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
 import Button from "@material-ui/core/Button";
 import CardMedia from "@material-ui/core/CardMedia";
+import { motion } from "framer-motion";
 
 const marketCard = (
   <Card sx={{ maxWidth: 345 }}>
@@ -86,11 +87,21 @@ function Catalog() {
           p={2}
         >
           <Grid item>
-            <Card>{marketCard}</Card>
+            <motion.button
+              whileHover={{ scale: 0.9 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Card>{marketCard}</Card>
+            </motion.button>
           </Grid>
 
           <Grid item>
-            <Card>{coursesCard}</Card>
+            <motion.button
+              whileHover={{ scale: 0.9 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              <Card>{coursesCard}</Card>
+            </motion.button>
           </Grid>
         </Grid>
       </Box>
