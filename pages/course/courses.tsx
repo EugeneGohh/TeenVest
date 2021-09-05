08701dc4ac0d914058cb4f5a49c141f46417137a
Eugene <gzkgt11@gmail.com>
2021-09-05 17:52:41 +0800

    update libraries & packages

--- pages/course/courses.tsx
+++ pages/course/courses.tsx
@@ -1,47 +1,78 @@
-import React from "react";
-import { experimentalStyled as styled } from "@mui/material/styles";
+import * as React from "react";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import NavBar from "../../components/core/NavBar";
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(2),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-
-function courses() {
+function Courses() {
   return (
     <Container maxWidth="xl" disableGutters>
       <Box
         sx={{
           bgcolor: "#FFFFFF",
           height: "auto",
+          display: "grid",
+          columnGap: "1",
         }}
       >
         <NavBar />
 
         <Grid
           container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
         >
-          <Grid item py={5}>
-            <Typography variant="h3" color="primary">
-              Courses
-            </Typography>
-          </Grid>
-        </Grid>
-
-        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
           {Array.from(Array(6)).map((index) => (
-            <Grid item xs={6} key={index} py={5}>
-              <Item>xs=2</Item>
+            <Grid item xs={12} sm={4} md={3} m={7} key={index}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardHeader
+                  avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                      R
+                    </Avatar>
+                  }
+                  action={
+                    <IconButton aria-label="settings">
+                      <MoreVertIcon />
+                    </IconButton>
+                  }
+                  title="Shrimp and Chorizo Paella"
+                  subheader="September 14, 2016"
+                />
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image="https://res.cloudinary.com/dfkd65uqz/image/upload/v1630025153/1_-_REBUILD_THE_ECONOMY_ygisvz.svg"
+                  alt="Paella dish"
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary">
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share">
+                    <ShareIcon />
+                  </IconButton>
+                </CardActions>
+              </Card>
             </Grid>
           ))}
         </Grid>
@@ -50,4 +81,4 @@ function courses() {
   );
 }
 
-export default courses;
+export default Courses;
